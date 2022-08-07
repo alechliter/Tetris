d2b85f54aa7c4d06463dfe00792fa420e230ca9e
Adam Lechliter <65375164+alechliter@users.noreply.github.com>
2022-08-06 20:05:07 -0400

    1-improved-layout - Added Dynamic Layout and Component interfaces
    Added a dynamic layout intereface based on the new design idea for displaying
    components without having to redisplay all components when an update is required.
    
    This design is intended to replace the original layout design for this story.

---
+++ Tetris_Console/interfaces/GameView/IComponent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lechliter.Tetris_Console
+{
+	public interface IComponent<TPoint, TGrid>
+	{
+		/// <summary>
+		/// The next ID to assign to a new component.
+		/// </summary>
+		public static int NextID { get; protected set; }
+
+		/// <summary>
+		/// The origin of the component.
+		/// </summary>
+		public TPoint Origin { get; set; }
+
+		/// <summary>
+		/// The layer number of the component in the layout.
+		/// </summary>
+		public int Layer { get; set; }
+
+		/// <summary>
+		/// The unique component identication index.
+		/// </summary>
+		public int ComponentID { get; protected set; }
+
+		/// <summary>
+		/// The contents of the component to display in a layout.
+		/// </summary>
+		public TGrid Grid { get; set; }
+
+		/// <summary>
+		/// An event that emits when the component needs to be displayed.
+		/// </summary>
+		public Action<int> Display { get; set; }
+
+		/// <summary>
+		/// A method that subscribes to the component's content provider.
+		/// </summary>
+		/// <param name="grid">The new grid content to display.</param>
+		public void OnUpdate(TGrid grid)
+		{
+			this.Display.Invoke(this.ComponentID);
+		}
+	}
+}
