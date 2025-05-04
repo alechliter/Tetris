d85674d242965129b4ff2c8785b83a715c6fa0c7
alechliter <adam.lechliter@gmail.com>
2025-05-04 13:52:50 -0400

    DEV-001.4 - Theme System - Initial Setup
    - Created theme utilities for defining theme colors and converting theme into theme variables per theme class.
    - Setup exporting scss assets from twa-core.
    - Setup twa-example and twa-tetris to import the twa-core styles.

--- TetrisWebApp/projects/twa-core/src/lib/example/example.component.ts
+++ TetrisWebApp/projects/twa-core/src/lib/example/example.component.ts
@@ -1,12 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+   ChangeDetectionStrategy,
+   Component,
+   HostBinding,
+   OnInit,
+   signal,
+   WritableSignal,
+} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
    selector: 'twa-lib-example',
    templateUrl: 'example.component.html',
    styleUrl: 'example.component.scss',
-   imports: [CommonModule],
+   changeDetection: ChangeDetectionStrategy.OnPush,
+   imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class ExampleComponent {
+   @HostBinding('style.color-scheme') get colorScheme(): string {
+      return this.currentColorScheme();
+   }
+
+   protected readonly selectedTheme: WritableSignal<string> = signal('');
+
+   protected readonly currentColorScheme: WritableSignal<string> = signal('light');
+
    constructor() {}
+
+   onToggleColorScheme(): void {
+      this.currentColorScheme.update(colorScheme => {
+         if (colorScheme === 'light') {
+            return 'dark';
+         } else {
+            return 'light';
+         }
+      });
+   }
+
+   onThemeChange(theme: string): void {
+      this.selectedTheme.set(`${theme}-theme`);
+   }
 }
